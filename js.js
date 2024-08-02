df0309f42665e0790c063867a5e445c8917af02c
Platon290109 <platon01.2009@gmail.com>
2024-08-02 20:44:37 +0300

    homework

---
+++ js.js
@@ -0,0 +1,38 @@
+const temperatureC = 22;
+const temperatureF = (22 * 9) / 5 + 32;
+console.log(temperatureF);
+
+const month = 30;
+const hours = month * 24;
+console.log(hours);
+const minute = hours * 60;
+console.log(minute);
+
+const hp = 100;
+const energy = 100;
+const troll = hp - 46;
+console.log(troll);
+const sprint = energy - 100;
+console.log(sprint);
+
+const candy = 500;
+const skidka = candy * 0.10;
+const candys = candy - skidka;
+console.log(candys);
+
+const number = Math.floor(0.2)
+console.log(number);
+
+const num = parseFloat('0.8qwerty')
+console.log(num);
+
+const no = parseInt('1qwer')
+console.log(no);
+
+const chips = Math.sqrt(25)
+console.log(chips);
+
+const baby = parseInt('14qwer')
+console.log(baby);
+const you = 14
+console.log(you.toString());
